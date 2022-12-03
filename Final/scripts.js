d500af8b8275e64a75643e452e76470bbaa1951b
Kayla Gervais <Gervais1217@MissouriState.edu>
2022-12-02 22:51:39 -0500

    updating

--- Final/scripts.js
+++ Final/scripts.js
@@ -7,11 +7,9 @@ var nyanWin= document.getElementById('nyanWin');
 var pusheenWin= document.getElementById('pusheenWin');
 var hamtaroWin= document.getElementById('hamtaroWin');
 var booWin= document.getElementById('booWin');
+var raceButton = document.getElementById('raceButton');
 
-let nyanChosen = false;
-let pusheenChosen = false;
-let hamtaroChosen = false;
-let booChosen = false;
+var selectedRacers = [];
 
 function ChangeLightColor() {
     var raceLight = document.getElementById("raceLight");
@@ -94,5 +92,35 @@ function randomNumber(min, max) {
 }
 
 function addRacer(racer){
+
+    const index = selectedRacers.indexOf(racer)
+    if (index > -1) {
+        // Already exists, remove the racer
+        selectedRacers.splice(index, 1);
+    }
+    else
+    {
+        // Add the racer if less than 2 racers are selected
+        if(selectedRacers.length < 2)
+        {
+            selectedRacers.push(racer);
+        }
+    }
+
+    //Set the race button to enabled or disabled based on the number of racers selected
+    if(selectedRacers.length == 2)
+    {
+        raceButton.removeAttribute('disabled');
+    }
+    else
+    {
+        raceButton.setAttribute('disabled',true);
+    }
+    console.log(selectedRacers);
+}
+
+function raceSetUp(){
+    window.location.href = "race.html";
+}
 
     
